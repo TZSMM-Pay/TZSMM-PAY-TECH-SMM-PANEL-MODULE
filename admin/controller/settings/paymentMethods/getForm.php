15408a5feb3fe7270c07b1129d0b6648645257d3
tzsmm <techzonesmm@gmail.com>
2025-01-30 18:21:22 +0600

    Update getForm.php

--- admin/controller/settings/paymentMethods/getForm.php
+++ admin/controller/settings/paymentMethods/getForm.php
@@ -1,1 +1,4 @@
-admin/controller/settings/paymentMethods/getForm.php
+if($method["methodId"] == 77) {
+    $form .= '<div class="form-group mb-3"><label class="form-label">Apikey</label>
+    <input type="text"  name="apikey" class="form-control" value="' . $methodExtras["apikey"] . '"/></div>';
+}
